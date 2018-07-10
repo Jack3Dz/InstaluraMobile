318200ff64996804f58efbaad5ce6e7fcc804ca9
Jefferson Bruchado <jefferson.damaceno17@gmail.com>
2018-07-09 22:33:16 -0300

    Curso 2 Parte 1

--- src/App.js
+++ src/App.js
@@ -24,14 +24,72 @@ export default class App extends Component {
     };
   };
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
       .then(resposta => resposta.json())
-      .then(json => this.setState({fotos: json}))
+      .then(json => this.setState({ fotos: json }))
       .catch(e => {
         console.warn('Não foi possível carregar as fotos: ' + e);
-        this.setState({status: 'ERRO'})
+        this.setState({ status: 'ERRO' })
       });
+  }
+
+  buscaPorId(idFoto) {
+    return this.state.fotos
+      .find(foto => foto.id === idFoto)
+  }
+
+  atualizaFotos(fotoAtualizada) {
+    const fotos = this.state.fotos
+      .map(foto => foto.id === fotoAtualizada.id ? fotoAtualizada : foto)
+    this.setState({ fotos })
+  }
+
+  like(idFoto) {
+    const foto = this.buscaPorId(idFoto);
+
+    let novaLista = [];
+    if (!foto.likeada) {
+
+      novaLista = [
+        ...foto.likers,
+        { login: 'meuUsuario' }
+      ]
+
+    } else {
+      novaLista = foto.likers.filter(liker => {
+        return liker.login !== 'meuUsuario'
+      })
+    }
+
+    const fotoAtualizada = {
+      ...foto,
+      likeada: !foto.likeada,
+      likers: novaLista
+    }
+
+    this.atualizaFotos(fotoAtualizada);
+  }
+
+  adicionaComentario(idFoto, valorComentario, inputComentario) {
+    if (valorComentario === '')
+      return;
+
+    const foto = this.buscaPorId(idFoto)
+
+    const novaLista = [...foto.comentarios, {
+      id: valorComentario,
+      login: 'meuUsuario',
+      texto: valorComentario
+    }];
+
+    const fotoAtualizada = {
+      ...foto,
+      comentarios: novaLista
+    }
+
+    this.atualizaFotos(fotoAtualizada);
+    inputComentario.clear();
   }
 
   render() {
@@ -39,8 +97,10 @@ export default class App extends Component {
       <FlatList style={styles.container}
         keyExtractor={item => String(item.id)}
         data={this.state.fotos}
-        renderItem={ ({ item }) =>
-          <Post foto={item}/>
+        renderItem={({ item }) =>
+          <Post foto={item}
+            likeCallback={this.like.bind(this)}
+            comentarioCallback={this.adicionaComentario.bind(this)} />
 
         }
       />
@@ -48,8 +108,9 @@ export default class App extends Component {
   }
 }
 
+const margin = Platform.OS === 'ios' ? 20 : 0;
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20
+    marginTop: margin
   }
 });
--- src/components/Post.js
+++ src/components/Post.js
@@ -6,100 +6,28 @@ import {
     Image,
     Dimensions,
     TouchableOpacity,
-    TextInput,
     FlatList
 } from 'react-native';
+import InputComentario from './InputComentario';
+import Likes from './Likes';
+import Comentario from './Comentario'
 
 const width = Dimensions.get('screen').width
 const height = Dimensions.get('screen').height
 
 // type Props = {};
 export default class Post extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            foto: this.props.foto,
-            valorComentario: ''
-        }
-    }
-
-    carregaIcone(likeada) {
-        return likeada ? require('../../resources/img/s2-checked.png') :
-            require('../../resources/img/s2.png')
-    }
-
-    like() {
-        const { foto } = this.state;
-
-        let novaLista = [];
-        if (!foto.likeada) {
-
-            novaLista = [
-                ...foto.likers,
-                { login: 'meuUsuario' }
-            ]
-
-        } else {
-            novaLista = foto.likers.filter(liker => {
-                return liker.login !== 'meuUsuario'
-            })
-        }
-
-        const fotoAtualizada = {
-            ...foto,
-            likeada: !foto.likeada,
-            likers: novaLista
-        }
-
-        this.setState({ foto: fotoAtualizada })
-    }
-
-    exibeLikes(likers) {
-        if (likers.length <= 0)
-            return;
-
-        return (
-            <Text style={styles.likes}>
-                {likers.length} {likers.length > 1 ? 'curtidas' : 'curtida'}
-            </Text>
-        );
-    }
-
     exibeLegenda(foto) {
         if (foto.comentario === '')
             return;
 
-        return (
-            <View style={styles.comentario}>
-                <Text style={styles.tituloComentario}>{foto.loginUsuario}</Text>
-                <Text>{foto.comentario}</Text>
-            </View>
-        )
-    }
-
-    adicionaComentario() {
-        if (this.state.valorComentario === '')
-            return;
-
-        const novaLista = [...this.state.foto.comentarios, {
-            id: this.state.valorComentario,
-            login: 'meuUsuario',
-            texto: this.state.valorComentario
-        }];
-
-        const fotoAtualizada = {
-            ...this.state.foto,
-            comentarios: novaLista
-        }
-
-        this.setState({ foto: fotoAtualizada, valorComentario: '' });
-        this.inputComentario.clear();
-
+        return <Comentario usuario={foto.loginUsuario}
+            texto={foto.comentario} />
     }
 
     render() {
-        const { foto } = this.state;
+        const { foto, likeCallback, comentarioCallback } = this.props;
+
         return (
             <View>
                 <View style={styles.cabecalho}>
@@ -111,42 +39,16 @@ export default class Post extends Component {
                     style={styles.foto} />
 
                 <View style={styles.rodape}>
-                    <TouchableOpacity onPress={this.like.bind(this)}>
-                        <Image style={styles.botaoDeLike}
-                            source={this.carregaIcone(foto.likeada)} />
-                    </TouchableOpacity>
-
-                    {this.exibeLikes(foto.likers)}
+                    <Likes foto={foto} likeCallback={likeCallback} />
                     {this.exibeLegenda(foto)}
 
-                    {/* {foto.comentarios.map(comentario =>
-                        <View style={styles.comentario} key={comentario.id}>
-                            <Text style={styles.tituloComentario}>{comentario.login}</Text>
-                            <Text>{comentario.texto}</Text>
-                        </View>
-                    )} */}
+                    {foto.comentarios.map(comentario =>
+                        <Comentario key={comentario.id}
+                            usuario={comentario.login} texto={comentario.texto} />
+                    )}
 
-                    <FlatList
-                        data={foto.comentarios}
-                        keyExtractor={item => item.id}
-                        renderItem={({ item }) =>
-                            <View style={styles.comentario}>
-                                <Text style={styles.tituloComentario}>{item.login}</Text>
-                                <Text>{item.texto}</Text>
-                            </View>
-                        } />
-
-                    <View style={styles.novoComentario}>
-                        <TextInput style={styles.input}
-                            placeholder="Adicione um comentário..."
-                            underlineColorAndroid='transparent'
-                            ref={input => this.inputComentario = input}
-                            onChangeText={text => this.setState({ valorComentario: text })} />
-                        <TouchableOpacity onPress={this.adicionaComentario.bind(this)}>
-                            <Image style={styles.icone}
-                                source={require('../../resources/img/send.png')} />
-                        </TouchableOpacity>
-                    </View>
+                    <InputComentario idFoto={foto.id}
+                        comentarioCallback={comentarioCallback} />
                 </View>
             </View>
         );
@@ -166,42 +68,11 @@ const styles = StyleSheet.create({
         width: 40,
         height: 40
     },
-
     foto: {
         width: width,
         height: height
     },
-    botaoDeLike: {
-        marginBottom: 10,
-        height: 40,
-        width: 40
-    },
     rodape: {
         margin: 10
-    },
-    likes: {
-        fontWeight: 'bold'
-    },
-    comentario: {
-        flexDirection: 'row'
-    },
-    tituloComentario: {
-        fontWeight: 'bold',
-        marginRight: 5
-    },
-    novoComentario: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        borderBottomWidth: 1,
-        borderBottomColor: '#ddd'
-    },
-    input: {
-        flex: 1,
-        height: 40,
-    },
-    icone: {
-        width: 30,
-        height: 30
     }
-
 });
